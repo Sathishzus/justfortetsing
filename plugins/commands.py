ff80e945e92b21c7b09ac7aaf5aff1459cab78e4
Sathishzus <73419683+Sathishzus@users.noreply.github.com>
2021-06-14 20:39:08 +0530

    Add files via upload

---
+++ plugins/commands.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# @trojanzhex
+
+
+from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+from script import script
+
+
